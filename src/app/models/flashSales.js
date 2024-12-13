865d33f8c402747cbc9240372df77541fa8fbe0d
dainji04 <ndai31072004@gmail.com>
2024-12-13 18:10:47 +0700

    change model flash sales

--- src/app/models/flashSales.js
+++ src/app/models/flashSales.js
@@ -5,14 +5,17 @@ const slug = require('mongoose-slug-generator');
 const flashSales = new schema(
     {
         // itemId: { type: String, required: true },
-        name: { type: String, required: true },
-        thumbnail: { type: String, required: true },
+        brand: { type: String, required: true },
+        colors: { type: Array, required: true },
         description: { type: String, required: true },
-        price: { type: Number, required: true },
-        discount: { type: Number, required: true },
-        stock: { type: Number, required: true },
         detailsImg: { type: Array, required: true },
+        discount: { type: Number, default: 0 },
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
         slug: { type: String, slug: 'name' },
+        stock: { type: Number, required: true },
+        thumbnail: { type: String, required: true },
+        type: { type: String, required: true },
     },
     {
         timestamps: true,
